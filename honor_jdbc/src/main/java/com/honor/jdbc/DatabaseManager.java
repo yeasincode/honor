abe555ed6ed77776fc4a412ed30da069e4f7d5d5
yeasin <yeasin@qq.com>
2019-06-05 09:28:43 +0800

    change

---
+++ honor_jdbc/src/main/java/com/honor/jdbc/DatabaseManager.java
@@ -0,0 +1,30 @@
+import org.springframework.jdbc.core.JdbcTemplate;
+
+/**
+ * <pre>
+ *
+ *  honor Technology
+ *  File: DatabaseManager
+ *
+ *  honor, Inc.
+ *  Copyright (C): 2019
+ *
+ *  Description:
+ *
+ *
+ *  Notes:
+ * 	$Id: DatabaseManager 72642 2009-01-01 20:01:57Z honor\Administrator $
+ *
+ *  Revision History
+ *  &lt;Date&gt;,			&lt;Who&gt;,			&lt;What&gt;
+ *  2019年04月26日		Administrator		Initial.
+ *
+ * </pre>
+ */
+public class DatabaseManager {
+    JdbcTemplate jdbcTemplate;
+}
+
+/*
+ *$Log: av-env.bat,v $
+ */
