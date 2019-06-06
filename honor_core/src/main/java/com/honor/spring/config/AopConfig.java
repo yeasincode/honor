b814c4e9412e77749446ca689320a9ad3585a966
yeasin <yeasin@qq.com>
2019-06-06 17:36:08 +0800

    test aop

--- honor_core/src/main/java/com/honor/spring/config/AopConfig.java
+++ honor_core/src/main/java/com/honor/spring/config/AopConfig.java
@@ -1,5 +1,6 @@
 package com.honor.spring.config;
 
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.context.annotation.Configuration;
@@ -9,7 +10,22 @@ import org.springframework.context.annotation.Configuration;
 public class AopConfig {
 
     @Pointcut("execution(* com.honor.*.business.*.*(..))")
-    public void executeService(){}
+    public void executeService() {
+        System.out.println("-----------executeService----------------");
+    }
 
+    @Around("executeService()")
+    public void aroundExecuteService() {
+        System.out.println("-----------aroundExecuteService----------------");
+    }
 
+    @Pointcut("execution(* com.honor.*.daoImpl.*.*(..))")
+    public void executeDao() {
+        System.out.println("-----------executeDao----------------");
+    }
+
+    @Around("executeService()")
+    public void aroundExecuteDao() {
+        System.out.println("-----------aroundExecuteDao----------------");
+    }
 }
