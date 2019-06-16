f80ca9d5e8f77e35a3800f9a367d8ac7e1444aa0
yeasincode <yeasinwork@163.com>
2019-06-17 00:59:31 +0800

    redis

--- honor_web/src/main/java/com/honor/web/WebApp.java
+++ honor_web/src/main/java/com/honor/web/WebApp.java
@@ -34,21 +34,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @PropertySource("classpath:redis.properties")
 @EnableScheduling
 public class WebApp {
-    @Profile("usage_message")
-    @Bean
-    public CommandLineRunner usage(){
-
-        return args -> {
-            System.out.println("This app uses Spring Profiles to control its behavior.");
-            System.out.println("Sample usage: java -jar rabbit-tutorials.jar --spring.profiles.active=hello-world,sender");
-        };
-    }
-    @Profile("!usage_message")
-    @Bean
-    public CommandLineRunner tutorial() {
-        return new RabbitAmqpTutorialsRunner();
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(WebApp.class,args);
     }
